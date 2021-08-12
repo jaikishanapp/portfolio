a2c8b4a5c6fc277a29119b325dc0fef5d7f9d7e8
Nitin Yadav <20nitinyadav78@gmail.com>
2021-08-12 16:54:03 +0530

    firebase realtime database added successfully

--- public/script.js
+++ public/script.js
@@ -70,3 +70,63 @@ $(document).ready(function(){
         }
     });
 });
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBaIi97C2pYNvQ4BufRqeDqY-RYL9FA3hw",
+    authDomain: "website-nitinyadav.firebaseapp.com",
+    databaseURL: "https://website-nitinyadav-default-rtdb.firebaseio.com",
+    projectId: "website-nitinyadav",
+    storageBucket: "website-nitinyadav.appspot.com",
+    messagingSenderId: "416280826495",
+    appId: "1:416280826495:web:756179e8ed1018f167e54a",
+    measurementId: "G-HK0P9D4JPC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+//Reference for form collection(3)
+let formMessage = firebase.database().ref('message');
+
+//listen for submit event//(1)
+document
+  .getElementById('contactus')
+  .addEventListener('submit', formSubmit);
+
+//Submit form(1.2)
+function formSubmit(e) {
+  e.preventDefault();
+  // Get Values from the DOM
+  let name = document.querySelector('#name').value;
+  let email = document.querySelector('#email').value;
+  let subject = document.querySelector('#subject').value;
+  let message = document.querySelector('#message').value;
+
+  //send message values
+  sendMessage(name, email, subject, message);
+
+  //Show Alert Message(5)
+  document.querySelector('.alert').style.display = 'block';
+
+  //Hide Alert Message After Seven Seconds(6)
+  setTimeout(function() {
+    document.querySelector('.alert').style.display = 'none';
+  }, 7000);
+
+  //Form Reset After Submission(7)
+  document.getElementById('contactus').reset();
+}
+
+//Send Message to Firebase(4)
+
+function sendMessage(name, email, subject, message) {
+  let newFormMessage = formMessage.push();
+  newFormMessage.set({
+    name: name,
+    email: email,
+    subject: subject,
+    message: message
+  });
+}
